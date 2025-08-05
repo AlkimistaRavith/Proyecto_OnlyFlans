da614645a84200d13429d206db551324197436a4
AlkimistaRavith <ravith.d3@gmail.com>
2025-08-05 00:53:25 +0000

    avance con hitos 3 y 4, pendentes en 2

--- web/views.py
+++ web/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
 def home(request):
-    return render(request, "home.html", {})
+    return render(request, "web/home.html", {})
+
+
+@login_required
+def home_premium(request):
+    return render(request, "web/home_premium.html", {})
